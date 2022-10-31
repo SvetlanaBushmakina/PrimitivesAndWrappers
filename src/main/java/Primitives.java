e9631385dcb5b08eac39fe8e066eae37bd26b209
Svetlana Bushmakina <svetik_5073@mail.ru>
2022-10-31 23:11:26 +0400

    PrimitivesAndWrappers

---
+++ src/main/java/Primitives.java
@@ -0,0 +1,14 @@
+public class Primitives {
+    public static void main(String[] args) {
+
+        boolean a = true;           // a = true
+        byte b = 3;                 // b = 3
+        short c = 8;                // c = 8
+        int d = 10;                 // d = 10
+        long e = 20000000000000L;   // e = 20000000000000
+        double f = 5.8;             // f = 5.8
+        float g = 4.9f;             // g = 4.9
+        char ch = 102;              // ch = f
+
+    }
+}
---
+++ src/main/java/Wrappers.java
@@ -0,0 +1,14 @@
+public class Wrappers {
+    public static void main(String[] args) {
+
+        Boolean a = true;
+        Byte b = 2;
+        Short c = 10;
+        Integer d = 15;
+        Long e = 3200000000000000L;
+        Double f = 35.7;
+        Float g = 4.9f;
+        Character ch = 115;
+
+    }
+}
